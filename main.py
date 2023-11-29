a1049c4fb868dbfad106a8cd09a56481a90a3ce8
namoxd101 <namo1011970z@gmail.com>
2023-11-29 11:36:58 +0700

    morebuttons1

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import tkinter as tk
 class MyCalculator:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("1080x480")
+        self.root.geometry("600x480")
         self.root.title("Calculator Pro X +++")
 
-        self.label = tk.Label(self.root, text="Calculator Pro X +++", font=('Comic Sans MS', 69))
+        self.label = tk.Label(self.root, text="Calculator Pro X +++", font=('Comic Sans MS', 40))
         self.label.pack()
-        self.button = tk.Button(self.root, text="Download Here", font=('Comic Sans MS', 10))
-        self.button.place(x=500, y=150)
+        self.button = tk.Button(self.root, text="CE", font=('Comic Sans MS', 25))
+        self.button.place(x=75, y=150)
+        self.button = tk.Button(self.root, text="/", font=('Comic Sans MS', 25))
+        self.button.place(x=190, y=150)
+        self.button = tk.Button(self.root, text="x", font=('Comic Sans MS', 25))
+        self.button.place(x=275, y=150)
+        self.button = tk.Button(self.root, text="-", font=('Comic Sans MS', 25))
+        self.button.place(x=360, y=150)
+        self.button = tk.Button(self.root, text="+", font=('Comic Sans MS', 25))
+        self.button.place(x=445, y=150)
+        self.button = tk.Button(self.root, text="7", font=('Comic Sans MS', 25))
+        self.button.place(x=85, y=240)
 
         self.root.mainloop()
 
