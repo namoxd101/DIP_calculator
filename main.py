1a8d1edb2a2932ca9d2a3a107d203f1987ce2453
mingggp <voravich2549@gmail.com>
2024-01-17 08:12:13 +0700

    Final for display monitor

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
 from tkinter import *
 
-'''
-Functions
-'''
-# Function to add in the entry of text display
-def button_click(char):
-    global calc_operator
-    calc_operator += str(char)
-    text_input.set(calc_operator)
-
-# Function to clear the whole entry of text display
-def button_clear_all():
-    global calc_operator
-    calc_operator = ""
-    text_input.set("")
-
-# Function to delete one by one from the last in the entry of text display
-def button_delete():
-    global calc_operator
-    text = calc_operator[:-1]
-    calc_operator = text
-    text_input.set(text)
-
-# Function to change the sign of number
-def sign_change():
-    global calc_operator
-    if calc_operator[0]=='-':
-        temp = calc_operator[1:]
-    else:
-        temp = '-'+calc_operator
-    calc_operator = temp
-    text_input.set(temp)    
-
-
 root = Tk()
 root.configure(bg="#293C4A", bd=10)
 root.title("Nerd Calculator")
