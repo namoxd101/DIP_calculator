b429476b73ab3ac9c6acdace3804191583d9e4bc
mingggp <voravich2549@gmail.com>
2024-01-17 07:59:46 +0700

    Kinda Final

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ root = Tk()
 root.configure(bg="#293C4A", bd=10)
 root.title("Nerd Calculator")
 
-calc_operator = ""
-text_input = StringVar()
-
-text_display = Entry(root, font=('sans-serif', 20, 'bold'), textvariable=text_input,
-                     bd=5, insertwidth = 5, bg='#BBB', justify='right').grid(columnspan=5, padx = 10, pady = 15)
 
 button_params = {'bd':5, 'fg':'#BBB', 'bg':'#3C3636', 'font':('sans-serif', 20, 'bold')}
 button_params_main = {'bd':5, 'fg':'#000', 'bg':'#BBB', 'font':('sans-serif', 20, 'bold')}
