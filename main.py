20f761534dfd98f9a341aa8df0f1a26dfb6a79b5
mingggp <voravich2549@gmail.com>
2024-01-17 08:26:13 +0700

    All functions

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import tkinter as tk
-class MyCalculator:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("1080x480")
-        self.root.title("Calculator Pro X +++")
-
-        self.label = tk.Label(self.root, text="Calculator Pro X +++", font=('Comic Sans MS', 69))
-        self.label.pack()
-        self.button = tk.Button(self.root, text="Download Here", font=('Comic Sans MS', 10))
-        self.button.place(x=500, y=150)
-
-        self.root.mainloop()
 from tkinter import *
 
-'''
-Functions
-'''
+root = Tk()
+root.configure(bg="#293C4A", bd=10)
+root.title("Nerd Calculator")
+
 # Function to add in the entry of text display
 def button_click(char):
     global calc_operator
@@ -60,88 +48,9 @@ def button_equal():
     calc_operator = temp_op
 
 
-
 root = Tk()
 root.configure(bg="#293C4A", bd=10)
 root.title("Nerd Calculator")
 
-calc_operator = ""
-text_input = StringVar()
-
-text_display = Entry(root, font=('sans-serif', 20, 'bold'), textvariable=text_input,
-                     bd=5, insertwidth = 5, bg='#BBB', justify='right').grid(columnspan=5, padx = 10, pady = 15)
-
-button_params = {'bd':5, 'fg':'#BBB', 'bg':'#3C3636', 'font':('sans-serif', 20, 'bold')}
-button_params_main = {'bd':5, 'fg':'#000', 'bg':'#BBB', 'font':('sans-serif', 20, 'bold')}
-
-'''
-Buttons
-'''
-
-
-#--1st row--
-# Add a left parentheses
-left_par = Button(root, button_params, text='(',
-                  command=lambda:button_click('(')).grid(row=5, column=0, sticky="nsew")
-# Add a right parentheses
-right_par = Button(root, button_params, text=')',
-                   command=lambda:button_click(')')).grid(row=5, column=1, sticky="nsew")   
-# Change the sign of a number
-signs = Button(root, button_params, text='\u00B1',
-               command=sign_change).grid(row=5, column=2, sticky="nsew")
-# Transform number to percentage
-percentage = Button(root, button_params, text='%',
-               command=percent).grid(row=5, column=3, sticky="nsew")
-# Calculate the function e^x
-ex = Button(root, button_params, text='e^x',
-               command=lambda:button_click('e(')).grid(row=5, column=4, sticky="nsew")
-
-#--2nd row--
-button_7 = Button(root, button_params_main, text='7',
-                  command=lambda:button_click('7')).grid(row=6, column=0, sticky="nsew")
-button_8 = Button(root, button_params_main, text='8',
-                  command=lambda:button_click('8')).grid(row=6, column=1, sticky="nsew")
-button_9 = Button(root, button_params_main, text='9',
-                  command=lambda:button_click('9')).grid(row=6, column=2, sticky="nsew")
-delete_one = Button(root, bd=5, fg='#000', font=('sans-serif', 20, 'bold'),
-              text='DEL', command=button_delete, bg='#db701f').grid(row=6, column=3, sticky="nsew")
-delete_all = Button(root, bd=5, fg='#000', font=('sans-serif', 20, 'bold'),
-              text='AC', command=button_clear_all, bg='#db701f').grid(row=6, column=4, sticky="nsew")
-
-#--3rd row--
-button_4 = Button(root, button_params_main, text='4',
-                  command=lambda:button_click('4')).grid(row=7, column=0, sticky="nsew")
-button_5 = Button(root, button_params_main, text='5',
-                  command=lambda:button_click('5')).grid(row=7, column=1, sticky="nsew")
-button_6 = Button(root, button_params_main, text='6',
-                  command=lambda:button_click('6')).grid(row=7, column=2, sticky="nsew")
-mul = Button(root, button_params_main, text='*',
-             command=lambda:button_click('*')).grid(row=7, column=3, sticky="nsew")
-div = Button(root, button_params_main, text='/',
-             command=lambda:button_click('/')).grid(row=7, column=4, sticky="nsew")
-
-#--4th row--
-button_1 = Button(root, button_params_main, text='1',
-                  command=lambda:button_click('1')).grid(row=8, column=0, sticky="nsew")
-button_2 = Button(root, button_params_main, text='2',
-                  command=lambda:button_click('2')).grid(row=8, column=1, sticky="nsew")
-button_3 = Button(root, button_params_main, text='3',
-                  command=lambda:button_click('3')).grid(row=8, column=2, sticky="nsew")
-add = Button(root, button_params_main, text='+',
-             command=lambda:button_click('+')).grid(row=8, column=3, sticky="nsew")
-sub = Button(root, button_params_main, text='-',
-             command=lambda:button_click('-')).grid(row=8, column=4, sticky="nsew")
-
-#--5th row--
-button_0 = Button(root, button_params_main, text='0',
-                  command=lambda:button_click('0')).grid(row=9, column=0, sticky="nsew")
-point = Button(root, button_params_main, text='.',
-               command=lambda:button_click('.')).grid(row=9, column=1, sticky="nsew")
-exp = Button(root, button_params_main, text='EXP', font=('sans-serif', 16, 'bold'),
-             command=lambda:button_click(E)).grid(row=9, column=2, sticky="nsew")
-equal = Button(root, button_params_main, text='=',
-               command=button_equal).grid(row=9, columnspan=2, column=3, sticky="nsew")
-
 
 root.mainloop()
-MyCalculator()
