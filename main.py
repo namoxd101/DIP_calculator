b69736b36f31d3f0a9c4103b4be2c983fa9e71a8
mingggp <voravich2549@gmail.com>
2024-01-17 08:28:03 +0700

    Final DEV

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-import tkinter as tk
-class MyCalculator:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("1080x480")
-        self.root.title("Calculator Pro X +++")
-
-        self.label = tk.Label(self.root, text="Calculator Pro X +++", font=('Comic Sans MS', 69))
-        self.label.pack()
-        self.button = tk.Button(self.root, text="Download Here", font=('Comic Sans MS', 10))
-        self.button.place(x=500, y=150)
-
-        self.root.mainloop()
 from tkinter import *
 
 '''
@@ -58,7 +45,6 @@ def button_equal():
     temp_op = str(eval(calc_operator))
     text_input.set(temp_op)
     calc_operator = temp_op
-
 
 
 root = Tk()
@@ -144,4 +130,3 @@ equal = Button(root, button_params_main, text='=',
 
 
 root.mainloop()
-MyCalculator()
